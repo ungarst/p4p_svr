4ffba2aaf396f26481d0ef871d60b54941e848cb
Dave Carpenter <dave@ungarst.com>
2013-06-19 15:15:15 +1200

    added price per item to a purchased item

--- models/purchased_item.py
+++ models/purchased_item.py
@@ -10,13 +10,15 @@ class PurchasedItem(Base):
   id = Column(Integer, primary_key=True)
   item_name = Column(String(200))
   quantity = Column(Integer)
+  price_per_unit = Column(Float)
   receipt_id = Column(Integer, ForeignKey('receipts.id'))
 
   #receipt = relationship("Receipt", 
   #          backref=backref('purchased_items', order_by=id))
 
-  def __init__(self, item_name, quantity):
+  def __init__(self, item_name, price_per_unit, quantity):
     self.item_name = item_name
+    self.price_per_unit = price_per_unit
     self.quantity = quantity
 
   def serialize(self):
