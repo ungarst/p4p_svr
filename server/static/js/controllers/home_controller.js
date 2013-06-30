87f93526669bb957e9330858918c8c7613ed13ce
Dave Carpenter <dave@ungarst.com>
2013-07-01 10:55:12 +1200

    ready to merge to master

--- server/static/js/controllers/home_controller.js
+++ server/static/js/controllers/home_controller.js
@@ -1,13 +1,23 @@
 function HomeCtrl ($scope, $location, $http) {
   
+  // request to confirm login
   $http.get('/login').success(function(data, status, headers, config) {
     $scope.data = data;
+    console.log(data);
     if (!("user" in $scope.data)) {
       $location.path('/');
     }
     $scope.user = $scope.data["user"];
-
     $scope.$parent.logged_in = true;
+
+    var receipts_url = '/users/' + $scope.user.user_id + '/receipts';
+
+    // request to get receipt data from the site
+    $http.get(receipts_url).success(function(data, status, headers, config) {
+      $scope.user_receipts = data["receipts"];
+    });  
+
+
   });
 
 
