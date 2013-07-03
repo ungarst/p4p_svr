a64e37e85f5d035155f274778846885ded52950c
Dave Carpenter <dave@ungarst.com>
2013-07-03 19:01:07 +1200

    Add item category changes

--- server/receiptspp.py
+++ server/receiptspp.py
@@ -159,6 +159,35 @@ def receipt(user_id, receipt_id):
 
     # return the serialized with items 
 
+@app.route('/users/<int:user_id>/receipts/<int:receipt_id>/purchased_items/<int:item_id>', methods=['PUT'])
+def item(user_id, receipt_id, item_id):
+    # perform some sort of validation to ensure that the item belongs to that receipt and that user
+
+    item = PurchasedItem.query.filter_by(id=item_id).first()
+
+    if not item:
+        return make_response("This item doesn't exist", 404)
+
+    receipt = item.receipt
+    if receipt.id != receipt_id:
+        return make_response("This item doesn't belong to the receipt specified", 404)
+
+    user = receipt.user
+    if user.id != user_id:
+        return make_response("The item doesn't belong to the user specified", 404)
+
+
+    if "category" in request.json:
+        print "her"
+        item.category = request.json["category"]
+        db.add(item)
+        db.commit()
+        return json.dumps({'item': item.serialize()})
+    else:
+        return make_response("Need category in the JSON", 404)
+    return "hello world"
+
+
 @app.route('/users/<int:user_id>/spending_categories', methods=["GET", "POST"])
 def spending_categories(user_id):
     if request.method == "POST":
