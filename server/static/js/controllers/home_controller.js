c9884ea8e6bd08e66826b5601b53c26262965abf
Dave Carpenter <dave@ungarst.com>
2013-07-10 17:34:25 +1200

    made the add receipts page work

--- server/static/js/controllers/home_controller.js
+++ server/static/js/controllers/home_controller.js
@@ -16,18 +16,6 @@ function HomeCtrl ($scope, $location, $http) {
     $location.path('/add_receipt');
   }
 
-  // prototyping functions to get todays date as dd/MM/yyyy and time as hh:mm:ss
-  Date.prototype.today = function(){ 
-    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
-  };
-
-  Date.prototype.timeNow = function(){
-    return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
-  };
-
-  var date = new Date();
-  var currentDateTime = String(date.today() + " " + date.timeNow());
-
   $scope.receipts_url = "";
 
   var getReceipts = function() {
@@ -61,43 +49,4 @@ function HomeCtrl ($scope, $location, $http) {
     });
   }
 
-  // this is just used so that the correct currency locale can be generated 
-  // for a place holder
-  $scope.price_example = 20;
-
-  //default receipt
-  $scope.new_receipt = {
-    "storeName": "",
-    "taxRate": "",
-    "totalTransaction": "",
-    "dateTime": currentDateTime,
-    "category" : "",
-    "items": [{"item":{"title":"Shirt","price":200},"itemQuantity":1}]
-  };
-
-  $scope.clearReceipt = function() {
-    $scope.new_receipt = {
-      "storeName":"",
-      "taxRate" : "",
-      "totalTransaction" : "",
-      "category" : "",
-      "dateTime": currentDateTime,
-      "items": [{"item":{"title":"Shirt","price":200},"itemQuantity":1}]
-    };
-  }
-
-  $scope.saveReceipt = function() {
-    $scope.new_receipt.totalTransaction = Number($scope.new_receipt.totalTransaction.
-      replace(/[^0-9\.]+/g,""));
-    $scope.new_receipt.taxRate = Number($scope.new_receipt.taxRate.
-      replace(/[^0-9\.]+/g,""));
-
-    $http.post($scope.receipts_url, $scope.new_receipt).success(function(data, status, headers, config) {
-      $('#addReceiptModal').modal('hide');
-      // request to get receipt data from the site
-      getReceipts(); 
-      $scope.clearReceipt(); 
-      $location.path('/home');
-    });
-  }
 }
