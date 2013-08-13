3a8d4361611bbe3ce46446fa38829584c3da34e4
Dave Carpenter <dave@ungarst.com>
2013-08-13 20:55:08 +1200

    implement editing

--- server/static/js/controllers/budgeting_tab_controllers.js
+++ server/static/js/controllers/budgeting_tab_controllers.js
@@ -1,50 +1,93 @@
 function BudgetingOverviewCtrl ($scope) {
 
-	$scope.onDataLoad = function() {
-		$scope.spending_categories_matrix = [];
+  $scope.onDataLoad = function() {
+    $scope.spending_categories_matrix = [];
 
-		for (var i = 0 ; i < $scope.spending_categories.length ; i++) {
-			if (!$scope.spending_categories_matrix[Math.floor(i/3)]) {
-				$scope.spending_categories_matrix.push([]);
-			}
-			$scope.spending_categories[i].ratio = (100 * $scope.spending_categories[i].monthly_spend / $scope.spending_categories[i].monthly_allowance).toFixed(2);
-			$scope.spending_categories_matrix[Math.floor(i/3)].push($scope.spending_categories[i]);
-		}
+    for (var i = 0 ; i < $scope.spending_categories.length ; i++) {
+      if (!$scope.spending_categories_matrix[Math.floor(i/3)]) {
+        $scope.spending_categories_matrix.push([]);
+      }
+      $scope.spending_categories[i].ratio = (100 * $scope.spending_categories[i].monthly_spend / $scope.spending_categories[i].monthly_allowance).toFixed(2);
+      $scope.spending_categories_matrix[Math.floor(i/3)].push($scope.spending_categories[i]);
+    }
 
-	};
+    $scope.user_other_ratio = ($scope.other / $scope.user.other_monthly_allowance).toFixed(2);
 
-	$scope.$on('SpendingCategoriesLoaded', $scope.onDataLoad);
+  };
 
-	if ($scope.dataIsLoaded) {
-		$scope.onDataLoad();
-	}
+  $scope.$on('SpendingCategoriesLoaded', $scope.onDataLoad);
+
+  if ($scope.dataIsLoaded) {
+    $scope.onDataLoad();
+  }
 }
 
 function BudgetingGraphsCtrl ($scope) {
-	$scope.count = 0;
+  $scope.count = 0;
 }
 
 function BudgetingEditCtrl ($scope, $http) {
-	
-	$scope.editCategory = function(category) {
+  
+  $scope.onDataLoad = function() {
+    for (var category in $scope.spending_categories) {
+      category.editing = false;
+    }
+  };
 
-	};
+  $scope.saveEdit = function(category) {
+    category.editing = false;
 
-	// Fuck confirmations for now
-	$scope.deleteCategory = function(category) {
-		// Pull out the id now for deleting from the API
-		var categoryId = category.spending_category_id;
+    // Send updates to backend
+    var url = "/users/" + $scope.user.user_id.toString() +
+        "/spending_categories/" + category.spending_category_id.toString();
+    var data = {"monthly_allowance": category.monthly_allowance };
 
-		// Remove it from the list now so that the user sees instant change
-		var index = $scope.spending_categories.indexOf(category);
-		$scope.spending_categories.splice(index, 1);
+    $http.put(url, data).success(function(data, status, headers, config){
+      $scope.refreshData();
+    });
+  };
 
-		// Remove the category from the backend
-		var url = "/users/" + $scope.user.user_id.toString() + "/spending_categories/" + categoryId.toString();
-		$http.delete(url).success(function(data, status, headers, config) {
-			$scope.refreshData();
-		});
+  $scope.editCategory = function(category) {
+    category.editing = true;
+  };
 
-	};
+  // Fuck confirmations for now
+  $scope.deleteCategory = function(category) {
+    // Pull out the id now for deleting from the API
+    var categoryId = category.spending_category_id;
+
+    // Remove it from the list now so that the user sees instant change
+    var index = $scope.spending_categories.indexOf(category);
+    $scope.spending_categories.splice(index, 1);
+
+    // Remove the category from the backend
+    var url = "/users/" + $scope.user.user_id.toString() + "/spending_categories/" + categoryId.toString();
+    $http.delete(url).success(function(data, status, headers, config) {
+      $scope.refreshData();
+    });
+
+  };
+
+  $scope.editUserCatchAll = function() {
+    $scope.editingCatchAll = true;
+  };
+
+  $scope.saveUserCatchAll = function() {
+    $scope.editingCatchAll = false;
+
+    // Save changes to backend
+    var url = "/users/" + $scope.user.user_id.toString();
+    var data = {"other_monthly_allowance": $scope.user.other_monthly_allowance};
+    $http.put(url, data).success(function(data, success, headers, config) {
+      $scope.refreshData();
+    });
+
+  };
+
+  $scope.$on('SpendingCategoriesLoaded', $scope.onDataLoad);
+
+  if ($scope.dataIsLoaded) {
+    $scope.onDataLoad();
+  }
 }
 
