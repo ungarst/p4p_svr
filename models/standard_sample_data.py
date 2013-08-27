bdbe05354fe61593364e72db93d262fab18a641c
Dave Carpenter <dave@ungarst.com>
2013-08-27 15:58:57 +1200

    Add dates to receipts generated by standard sample data script
    Previously sample receipts were created with the time being datetime.now. This made testing of the amount of data used to fetch receipts difficult as fetching receipts after a certain date was redundant because all receipts were on the same date.
    This change fixes this by incrementing the date assigned to sample receipts every second receipt

--- models/standard_sample_data.py
+++ models/standard_sample_data.py
@@ -1,13 +1,18 @@
 from models import db, User, Receipt, PurchasedItem
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def add_receipt(num_receipts=1):
 	user = User.query.first()
+
+	days_ago_to_start = num_receipts/2
+	date = datetime.now() - timedelta(days=days_ago_to_start)
+	even_receipt = True
+
 	for x in range(num_receipts):
 		SHOP_NAME = "Countdown Birkenhead"
 		SHOP_CATEGORY = "Groceries"
 		TOTAL = 123.81
-		r = Receipt(SHOP_NAME, SHOP_CATEGORY, TOTAL, datetime.now())
+		r = Receipt(SHOP_NAME, SHOP_CATEGORY, TOTAL, date)
 		r.purchased_items = [
 			    PurchasedItem("Chocolate Milk", SHOP_CATEGORY, 3.99, 2),
 			    PurchasedItem("Garlic Pita Breads", SHOP_CATEGORY, 4.50, 1),
@@ -19,6 +24,10 @@ def add_receipt(num_receipts=1):
 			    PurchasedItem("Olivio Butter", SHOP_CATEGORY, 5.95, 1)
        					 	]
 		user.receipts.append(r)
+
+		even_receipt = not even_receipt
+		if even_receipt:
+			date = date + timedelta(days=1)
 		print len(user.receipts)
 
 	db.add(user)
